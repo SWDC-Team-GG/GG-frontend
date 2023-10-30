4dc2238fa3a48c5977da41c048df53617da901ca
김규민 <82032508+wbhaao@users.noreply.github.com>
2023-10-30 21:28:57 +0900

    add : header

--- src/components/section/Header/index.tsx
+++ src/components/section/Header/index.tsx
@@ -1,7 +1,15 @@
+import { VscArrowLeft } from "react-icons/vsc";
 import React from "react";
+import * as S from "./style";
 
 function Header() {
-  return <div>헤더</div>;
+  return (
+    <S.Header>
+      <VscArrowLeft onClick={() => {}} size={24} style={{ color: "#3084f2" }} />
+      <S.Title>타이틀</S.Title>
+      <VscArrowLeft size={24} style={{ visibility: "hidden" }} />
+    </S.Header>
+  );
 }
 
 export default Header;
--- src/components/section/Header/style.ts
+++ src/components/section/Header/style.ts
@@ -1,1 +1,20 @@
 import styled from "styled-components";
+
+export const Header = styled.div`
+  width: 100%;
+  height: 40px;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  background-color: white;
+  border-bottom: 2px solid #3084f2;
+  box-shadow: 0px 0px 20px -10px #3084f2;
+  justify-content: space-between;
+`;
+
+export const Title = styled.p`
+  color: #3084f2;
+  font-weight: 1000;
+  text-align: center;
+  font-size: 1.2rem;
+`;

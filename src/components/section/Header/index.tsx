a2b276b4651b4267b8b58607903c9f0c4c084ba8
김규민 <82032508+wbhaao@users.noreply.github.com>
2023-10-30 22:25:40 +0900

    add : HEADER

--- src/components/section/Header/index.tsx
+++ src/components/section/Header/index.tsx
@@ -5,8 +5,15 @@ import * as S from "./style";
 function Header() {
   return (
     <S.Header>
-      <VscArrowLeft onClick={() => {}} size={24} style={{ color: "#3084f2" }} />
+      <VscArrowLeft
+        className="arrow"
+        onClick={() => {}}
+        size={24}
+        style={{ color: "#3084f2" }}
+      />
       <S.Title>타이틀</S.Title>
+      <S.NavTitle>단어 사전</S.NavTitle>
+      <S.NavTitle>퀴즈</S.NavTitle>
       <VscArrowLeft size={24} style={{ visibility: "hidden" }} />
     </S.Header>
   );
--- src/components/section/Header/style.ts
+++ src/components/section/Header/style.ts
@@ -1,20 +1,34 @@
 import styled from "styled-components";
 
 export const Header = styled.div`
+  @media screen and (max-width: 500px) {
+    justify-content: space-between;
+  }
   width: 100%;
-  height: 40px;
-  padding: 10px;
+  height: 70px;
+  padding: 10px 10px 10px 30px;
   display: flex;
   align-items: center;
   background-color: white;
   border-bottom: 2px solid #3084f2;
   box-shadow: 0px 0px 20px -10px #3084f2;
-  justify-content: space-between;
+  .arrow {
+    @media screen and (min-width: 500px) {
+      display: none;
+    }
+  }
 `;
 
 export const Title = styled.p`
   color: #3084f2;
-  font-weight: 1000;
+  font-weight: 400;
   text-align: center;
   font-size: 1.2rem;
+`;
+
+export const NavTitle = styled(Title)`
+  padding-left: 30px;
+  @media screen and (max-width: 500px) {
+    display: none;
+  }
 `;

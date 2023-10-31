6bef9b4e70f5df3b89914757874d49a3ddc21f6b
김규민 <82032508+wbhaao@users.noreply.github.com>
2023-10-31 16:02:35 +0900

    fix : survey 레이아웃 수정

--- src/pages/Survey/index.tsx
+++ src/pages/Survey/index.tsx
@@ -5,9 +5,9 @@ function Home() {
   const exp = ["초등학교", "중학교", "고등학교"];
   return (
     <S.Container>
-      <S.SubTitle>1 / 2</S.SubTitle>
+      <S.SubTitle>2 / 2</S.SubTitle>
       <S.Title>해당하는 사항에 체크해주세요</S.Title>
-      <form method="get" action="form-action.html">
+      <form>
         <S.DescTitle>자신의 학력</S.DescTitle>
         {exp.map((item) => (
           <S.CheckLabel>
@@ -15,6 +15,11 @@ function Home() {
             {item}
           </S.CheckLabel>
         ))}
+        <S.ButtonContainer>
+          <S.Before>이전</S.Before>
+          {/* color를 통해 이전, 다음 구분 */}
+          <S.After>다음</S.After>
+        </S.ButtonContainer>
       </form>
     </S.Container>
   );
--- src/pages/Survey/style.ts
+++ src/pages/Survey/style.ts
@@ -2,21 +2,21 @@ import styled from "styled-components";
 
 export const CheckLabel = styled.div`
   display: block;
-  font-size: 20px;
+  font-size: 16px;
   margin-top: 30px;
 `;
 
 export const Input = styled.input`
   margin-right: 10px;
-  width: 16px;
-  height: 16px;
+  width: 14px;
+  height: 14px;
   border-radius: 24px;
 `;
 
 export const SubTitle = styled.div`
   text-align: center;
   font-size: 16px;
-  margin: 30px 0px 5px 0px;
+  margin: 20px 0px 5px 0px;
 `;
 
 export const Title = styled.div`
@@ -28,12 +28,39 @@ export const Title = styled.div`
 
 export const DescTitle = styled(Title)`
   text-align: left;
-  font-size: 22px;
+  font-size: 20px;
   font-weight: 900;
   margin-bottom: 20px;
 `;
 
 export const Container = styled.div`
-  width: 370px;
-  margin: 0 auto;
+  width: 470px;
+  margin: 60px auto 0 auto;
+  border-radius: 10px;
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
+  padding: 1px 30px 40px 30px;
+`;
+
+export const Before = styled.button`
+  width: 100px;
+  background-color: lightgray;
+  border: none;
+  padding: 20px 0;
+  border-radius: 8px;
+  font-size: 16px;
+  color: white;
+`;
+
+export const After = styled(Before)`
+  width: 300px;
+  background-color: #3084f2;
+  margin-right: 10;
+  border-radius: 8px;
+  font-size: 16px;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  margin-top: 50px;
+  justify-content: space-between;
 `;

904dcd266e500916a92f2f5b7436bed89795ff8c
jyh071116 <jyh071116@gmail.com>
2023-11-02 04:41:01 +0900

    feat: useUser 훅 제작

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,38 @@
 import React from "react";
+import { ToastContainer } from "react-toastify";
+import useUser from "hooks/useUser";
 import logo from "assets/logo.svg";
 import * as S from "./style";
 
 function Header() {
+  const { user, isLogined, logout } = useUser();
   return (
-    <S.Layout>
-      <S.LogoBox to="/">
-        <S.LogoImg src={logo} />
-        <S.LogoText>readable</S.LogoText>
-      </S.LogoBox>
-      <S.NavigationBox>
-        <S.Navigation to="/quiz">퀴즈</S.Navigation>
-        <S.Navigation to="/dict">검색기록&사전</S.Navigation>
-      </S.NavigationBox>
-      <S.AccountBox>
-        <S.Navigation to="/login">로그인</S.Navigation>
-        <S.Navigation to="/signin">회원가입</S.Navigation>
-      </S.AccountBox>
-    </S.Layout>
+    <>
+      <S.Layout>
+        <S.LogoBox to="/">
+          <S.LogoImg src={logo} />
+          <S.LogoText>readable</S.LogoText>
+        </S.LogoBox>
+        <S.NavigationBox>
+          <S.Navigation to="/quiz">퀴즈</S.Navigation>
+          <S.Navigation to="/dict">검색기록&사전</S.Navigation>
+        </S.NavigationBox>
+        <S.AccountBox>
+          {isLogined ? (
+            <>
+              <div>{user?.name}님</div>
+              <div onClick={logout}>로그아웃</div>
+            </>
+          ) : (
+            <>
+              <S.Navigation to="/login">로그인</S.Navigation>
+              <S.Navigation to="/signin">회원가입</S.Navigation>
+            </>
+          )}
+        </S.AccountBox>
+      </S.Layout>
+      <ToastContainer />
+    </>
   );
 }
 

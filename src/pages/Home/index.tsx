f150d4f5f9a6512d46738fadd51939ed850c2f04
jyh071116 <jyh071116@gmail.com>
2023-11-02 08:31:09 +0900

    feat: 복사 기능, 카메라 아이콘 추가

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,9 @@
 import React, { useRef } from "react";
 import IWordprops from "interfaces/IWordprops";
 import Word from "components/Word";
-import { getUser } from "api/allGets";
+import camera from "assets/camera.png";
 import copy from "assets/copy.png";
+import { ToastContainer, toast } from "react-toastify";
 import * as S from "./style";
 
 function Home() {
@@ -49,45 +50,67 @@ function Home() {
     textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
   };
 
+  const handleCopy = async (text: string | undefined) => {
+    if (typeof text === "undefined") {
+      return toast.error("텍스트가 없습니다!");
+    }
+    try {
+      await navigator.clipboard.writeText(text);
+      return toast.success("복사되었습니다");
+    } catch {
+      return toast.error("복사 실패");
+    }
+  };
+
   return (
-    <S.Layout>
-      <S.Container>
-        <S.TranslateBox>
-          <S.InputBox>
-            <S.TranslateType>원문</S.TranslateType>
-            <S.TextBox>
-              <S.Textarea
-                rows={1}
-                ref={textareaRef}
-                onChange={() => handleTextareaHeight()}
-                placeholder="번역할 내용을 입력하세요."
-              />
-            </S.TextBox>
-            <S.ButtomNavBox>
-              <S.ButtomNav>
-                <S.Img src={copy} />
-              </S.ButtomNav>
-              <S.TranslateButton>번역하기</S.TranslateButton>
-            </S.ButtomNavBox>
-          </S.InputBox>
-          <S.InputBox>
-            <S.TranslateType>번역문</S.TranslateType>
-            <S.TextBox>{translateText}</S.TextBox>
-            <S.ButtomNavBox>
-              <S.ButtomNav>
-                <S.Img src={copy} />
-              </S.ButtomNav>
-            </S.ButtomNavBox>
-          </S.InputBox>
-        </S.TranslateBox>
-        <S.Line />
-        <S.WordBox>
-          {data.map((item) => (
-            <Word item={item} key={item.id} />
-          ))}
-        </S.WordBox>
-      </S.Container>
-    </S.Layout>
+    <>
+      <S.Layout>
+        <S.Container>
+          <S.TranslateBox>
+            <S.InputBox>
+              <S.TranslateType>원문</S.TranslateType>
+              <S.TextBox>
+                <S.Textarea
+                  rows={1}
+                  ref={textareaRef}
+                  onChange={() => handleTextareaHeight()}
+                  placeholder="번역할 내용을 입력하세요."
+                />
+              </S.TextBox>
+              <S.ButtomNavBox>
+                <S.Addons>
+                  <S.ButtomNav
+                    onClick={() => handleCopy(textareaRef.current?.value)}
+                  >
+                    <S.Img src={copy} />
+                  </S.ButtomNav>
+                  <S.ButtomNav>
+                    <S.Img src={camera} />
+                  </S.ButtomNav>
+                </S.Addons>
+                <S.TranslateButton>번역하기</S.TranslateButton>
+              </S.ButtomNavBox>
+            </S.InputBox>
+            <S.InputBox>
+              <S.TranslateType>번역문</S.TranslateType>
+              <S.TextBox>{translateText}</S.TextBox>
+              <S.ButtomNavBox>
+                <S.ButtomNav onClick={() => handleCopy(translateText)}>
+                  <S.Img src={copy} />
+                </S.ButtomNav>
+              </S.ButtomNavBox>
+            </S.InputBox>
+          </S.TranslateBox>
+          <S.Line />
+          <S.WordBox>
+            {data.map((item) => (
+              <Word item={item} key={item.id} />
+            ))}
+          </S.WordBox>
+        </S.Container>
+      </S.Layout>
+      <ToastContainer />
+    </>
   );
 }
 
